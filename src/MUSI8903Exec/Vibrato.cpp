14ef793037dd6bb84cbccc234c49793b6a8ee90b
Ashis Pati <ashis.pati.89@gmail.com>
2016-02-06 15:06:49 -0500

    Changed class structure for Vibrato class

--- src/MUSI8903Exec/Vibrato.cpp
+++ src/MUSI8903Exec/Vibrato.cpp
@@ -19,7 +19,13 @@
 static const char*  kVibratoBuildDate             = __DATE__;
 
 
-Vibrato::Vibrato () {}
+Vibrato::Vibrato (int max_delay_width, int num_channels)
+{
+    _ring_delay_line = new CRingBuffer<float>*[num_channels];
+    for (int i = 0; i < _num_channels; i++) {
+        _ring_delay_line[i] = new CRingBuffer<float>(_max_delay_width);
+    }
+}
 
 
 Vibrato::~Vibrato ()
@@ -54,14 +60,16 @@ const char*  Vibrato::getBuildDate ()
     return kVibratoBuildDate;
 }
 
-Error_t Vibrato::create (Vibrato*& pVibrato, float mod_freq, int delay_width, int mod_amp)
+Error_t Vibrato::create (Vibrato*& pVibrato, float max_delay_width_sec, long int sample_rate, int num_channels)
 {
-    pVibrato = new Vibrato ();
+    pVibrato->convertTimeToSamples(max_delay_width_sec, pVibrato->_max_delay_width);
+    pVibrato->_num_channels = num_channels;
+    pVibrato->_sample_rate = sample_rate;
+    pVibrato = new Vibrato (pVibrato->_max_delay_width, num_channels);
     
     if (!pVibrato)
         return kUnknownError;
 
-    pVibrato->init(mod_freq, delay_width, mod_amp);
     return kNoError;
 }
 
@@ -73,27 +81,39 @@ Error_t Vibrato::destroy (Vibrato*& pVibrato)
     pVibrato->reset ();
     
     delete pVibrato->_sin_osc;
-    delete pVibrato->_ring_delay_line;
+    for (int i = 0; i < pVibrato->_num_channels; i++) {
+        delete pVibrato->_ring_delay_line[i];
+    }
+    delete[] pVibrato->_ring_delay_line;
     delete pVibrato;
     pVibrato = 0;
     
     return kNoError;
 }
 
-Error_t Vibrato::init(float mod_freq, int delay_width, int mod_amp)
+Error_t Vibrato::init(float mod_freq, int delay_width_secs, int mod_amp_secs)
 {
     reset();
     
-    // check for parameters
-    if (mod_amp > delay_width)
-        return kFunctionInvalidArgsError;
-        
-    // initialise parameters and allocate memory
+    // initialise parameters
     _mod_freq = mod_freq;
-    _delay_width = delay_width;
-    _mod_amp = mod_amp;
-    _ring_delay_line = new CRingBuffer<float>(2*delay_width+1);
+    convertTimeToSamples(delay_width_secs, _delay_width);
+    convertTimeToSamples(mod_amp_secs, _mod_amp);
     _sin_osc = new LFO(mod_freq);
+    
+    // check parameter validity
+    if (_mod_amp > _delay_width)
+        return kFunctionInvalidArgsError;
+    
+    if (2*_delay_width+1 > _max_delay_width)
+        return kFunctionInvalidArgsError;
+
+    // set ring buffer write index
+    int delay_length = _delay_width + _mod_amp + 1;
+    for (int i = 0; i < _num_channels; i++) {
+        
+        _ring_delay_line[i]->setWriteIdx(_ring_delay_line[i]->getReadIdx() + delay_length);
+    }
     
     return kNoError;
 }
@@ -103,11 +123,21 @@ Error_t Vibrato::reset ()
     // reset buffers and variables to default values
     _mod_freq = 0.F;
     _delay_width = 0;
+    _mod_amp = 0;
     _sin_osc->setOscFreq(0);
-    _ring_delay_line->reset();
+    for (int i = 0; i < _num_channels; i++) {
+        _ring_delay_line[i]->reset();
+    }
     return kNoError;
 }
 Error_t Vibrato::process (float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames)
 {
+    
+    return kNoError;
+}
+
+Error_t Vibrato::convertTimeToSamples(int paramValue, int& param)
+{
+    param = paramValue*_sample_rate;
     return kNoError;
 }
--- src/inc/Vibrato.h
+++ src/inc/Vibrato.h
@@ -27,24 +27,31 @@ public:
     
     static const int  getVersion (const Version_t eVersionIdx);
     static const char* getBuildDate ();
-    
-    static Error_t create (Vibrato*& pCKortIf,float mod_freq, int delay_width, int mod_amp);
+    static Error_t create (Vibrato*& pCKortIf, float max_delay_width_sec, long int sample_rate, int num_channels);
     static Error_t destroy (Vibrato*& pCKortIf);
     
     Error_t init (float mod_freq, int delay_width, int mod_amp);
     Error_t reset ();
-    
     Error_t process (float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames);
+    Error_t setDelayWidth(float delay_width_secs);
+    Error_t setModAmp(float mod_amp_secs);
+    Error_t setModFreq(float mod_freq);
     
 private:
-    Vibrato ();
+    Vibrato (int max_delay, int num_channels);
     virtual ~Vibrato ();
     
-    CRingBuffer<float>* _ring_delay_line;
+    CRingBuffer<float>** _ring_delay_line;
     LFO* _sin_osc;
     float _mod_freq;
     int _delay_width;
+    int _max_delay_width;
     int _mod_amp;
+    long int _sample_rate;
+    int _num_channels;
+   
+    
+    Error_t convertTimeToSamples(int paramValue, int& param);
 };
 
 
